af8caaa236c7a1163887085b9f6ded3fcbf3b9b5
LKJNNG <34412677+linkangjing@users.noreply.github.com>
2018-04-05 01:44:18 +0800

    write your meaaage

---
+++ static/js/bus.js
@@ -0,0 +1,8 @@
+/*
+* @Author: Administrator
+* @Date:   2018-02-28 20:48:15
+* @Last Modified by:   Administrator
+* @Last Modified time: 2018-02-28 20:49:05
+*/
+import Vue from 'vue'
+export default new Vue;
---
+++ static/js/test.js
@@ -0,0 +1,9 @@
+/*
+* @Author: Administrator
+* @Date:   2018-02-27 18:01:41
+* @Last Modified by:   Administrator
+* @Last Modified time: 2018-02-27 18:07:06
+*/
+
+const IS_DEBUG = true //如果是测试环境就是true，如果是生产环境就是false
+const apiss = IS_DEBUG ? '':'http://120.76.205.241:8000/'
